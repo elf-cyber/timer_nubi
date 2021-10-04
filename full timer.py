961c10a86de70a4b285b7323bf2aa6d8dc253ac8
elf <87319528+elf-cyber@users.noreply.github.com>
2021-10-04 22:56:25 +0330

    Update full timer.py

--- full timer.py
+++ full timer.py
@@ -5,7 +5,11 @@ import colorama
 import random
 
 
-g = '''
+
+while True:
+            
+    def bunner():
+    a ='''
       
       wWw W  W    wWw        oo_    wWw    c  c  wWw  wWw()_()wW  Ww(o)__(o)wWw  wWw       c  cwWw  wWw___   wWw()_()  
       (O)(O)(O)   (O)_      /  _)-< (O)_   (OO)  (O)  (O)(O o)(O)(O)(__  __)(O)  (O)       (OO)(O)  (O|___)__(O)(O o)  
@@ -15,14 +19,33 @@ g = '''
      \ \_ (.-.__) /         ,-'   |\ \_ '.    ) '. `--' .`)|\\(_/\_)   )/    _/ /       '.    ) _/ /  | |_))\ \_ )|\\  
       \__) `-'  )/         (_..--'  \__)  `-.'    `-..-' (/  \)       (     (_.'          `-.' (_.'   (.'-'  \__|/  \      
          
-'''         
-    
-while True:
-      
+         
+         ''' 
+    i = random.randint(1,10)
+
+        if i == 1:
+            print(colorama.Fore.GREEN+a)
+        if i == 2: 
+            print(colorama.Fore.RED+colorama.Cursor.BACK()+a)
+        if i ==3 :
+            print(colorama.Fore.BLUE+colorama.Style.DIM+a)    
+        if i == 4:
+            print(colorama.Fore.CYAN+colorama.Cursor.BACK(a))
+        if i == 5:
+            print(colorama.Fore.YELLOW+a)
+        if i == 6:
+            print(colorama.Fore.LIGHTWHITE_EX+colorama.Style.DIM+colorama.Cursor.BACK(a))
+        if i == 7:
+            print(colorama.Fore.MAGENTA+a)
+        if i == 8:
+            print(colorama.Fore.LIGHTCYAN_EX+a)    
+        if i == 9:
+            print(colorama.Fore.CYAN+colorama.Style.DIM+a)
+        if i ==10:
+            print(colorama.Fore.LIGHTBLACK_EX+colorama.Style.DIM+a)
+        bunner()    
         a = time.localtime()
-    
         c = time.strftime(colorama.Fore.CYAN+colorama.Style.BRIGHT+'\t"year is":"%Y\n\t\t"mon is":"%m\n\t\t\t"day is":"%d"\n\t\t"yaer day":"%j"\n\t"monts":"%b"\n\n\n"%Z"\n\n\n\n\t\t\t}' , a)
-    
         b = time.strftime(colorama.Fore.CYAN+colorama.Style.BRIGHT+'{ \n\n"time is":"%H:%M:%S"\n' , a )
         print(g)
         print(b , c)
